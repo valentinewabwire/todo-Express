a6f6211d466271afbcc569f87fc080bfaff8e510
valentinewabwire <valentine.w.maita@gmail.com>
2022-09-21 15:43:13 +0300

    fetch from db

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const moongose = require("mongoose");
 
 // console.log(date());
 
@@ -11,12 +12,49 @@ const workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static("public"));
+moongose.connect("mongodb://localhost:27017/todolistDB");
 
+const itemSchema = new moongose.Schema({
+  name: String,
+});
+
+const Item = moongose.model("Item", itemSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!",
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item",
+});
+const item3 = new Item({
+  name: "<--- Hit this to delete an item",
+});
+
+const defaultItems = [item1, item2, item3];
+
+
+
+/* This is a route handler. It is listening for a get request to the root route. When it gets a
+request, it renders the list.ejs file and passes in the listTitle and newListItems variables. */
 app.get("/", function (req, res) {
-  let day = date.getDate();
-  res.render("list", { listTitle: day, newListItems: items });
+  
+  Item.find({}, function (err, foundItems) {
+    if (foundItems.length === 0) {
+      /* Inserting the default items into the database. */
+Item.insertMany(defaultItems, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully loaded the data to db");
+  }
+});
+res.redirect("/")
+    }else{
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
+    
+  });
 });
 
 app.get("/work", function (req, res) {
