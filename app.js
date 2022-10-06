c3143a1bf253012a31aa38a59ede18448015e346
valentinewabwire <valentine.w.maita@gmail.com>
2022-10-06 12:28:12 +0300

    git ignore

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ const workItems = [];
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-moongose.connect("mongodb://localhost:27017/todolistDB");
+moongose.connect(
+  "mongodb+srv://admin-shifu:Data2020.@cluster0.buxbmwi.mongodb.net/todolistDB"
+);
 
 const itemSchema = new moongose.Schema({
   name: String,
@@ -33,27 +35,23 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
-
-
 /* This is a route handler. It is listening for a get request to the root route. When it gets a
 request, it renders the list.ejs file and passes in the listTitle and newListItems variables. */
 app.get("/", function (req, res) {
-  
   Item.find({}, function (err, foundItems) {
     if (foundItems.length === 0) {
       /* Inserting the default items into the database. */
-Item.insertMany(defaultItems, function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Successfully loaded the data to db");
-  }
-});
-res.redirect("/")
-    }else{
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully loaded the data to db");
+        }
+      });
+      res.redirect("/");
+    } else {
       res.render("list", { listTitle: "Today", newListItems: foundItems });
     }
-    
   });
 });
 
@@ -68,17 +66,58 @@ app.post("/work", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  let item = req.body.todoItem;
+  const itemName = req.body.todoItem;
 
-  if (req.body.list === "work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item({
+    name: itemName,
+  });
+
+  item.save();
+
+  res.redirect("/");
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  //var checkedItemId = moongose.Types.ObjectId(req.params.checkbox);
+
+  //const test = moongose.isValidObjectId(checkedItemId);
+  const _id = moongose.Types.ObjectId("632c01263d637bd3d4f7ae14");
+  //const test = moongose.isValidObjectId(id);
+  //console.log(checkedItemId);
+
+  console.log(_id);
+  //checkedItemId.toString()
+
+  //console.log(req.body.checkbox);
+
+  // Item.findByIdAndDelete(req.body.checkbox,function(err, docs){
+  //   if (!err) {
+  //     console.log(docs);
+  //   }else{
+  //     console.log(err);
+  //   }
+  // });
+
+  Item.findByIdAndRemove(req.params.checkbox, function (err, docs) {
+    // console.log(checkedItemId);
+    //console.log(err);
+    // console.log(docs);
+    if (!err) {
+      //console.log("User with ID " + checkedItemId + " deleted successfuly");
+      //res.redirect("/");
+    } else {
+      //console.log(err);
+      // res.redirect("/");
+    }
+  });
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("Server started has started succesfully");
 });
